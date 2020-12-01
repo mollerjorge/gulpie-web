7eadd65cb22ea660733295e20ebd39ae3072a499
mollerjorge <jmoller2106@gmail.com>
2020-11-30 22:45:51 -0300

    Add typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,16 @@ module.exports = {
     extend: {
       colors: {
         turqoise: colors.cyan,
-        gray: colors.blueGray
+        gray: colors.blueGray,
+        black: {
+          dark: '#333333',
+          DEFAULT: '#858A8F',
+          light: '#AAB0B7'
+        }
       }
+    },
+    fontFamily: {
+      inter: 'Inter, sans-serif'
     }
   },
   variants: {
